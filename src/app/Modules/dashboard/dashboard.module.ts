5d26cb2bd7d49a58e78ff4683ffdc4987623ed2d
harshit740 <harsh.008.com@gmail.com>
2019-10-16 08:15:55 +0530

    Added Individual modules

--- src/app/Modules/dashboard/dashboard.module.ts
+++ src/app/Modules/dashboard/dashboard.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { DemoMaterialModule } from 'src/app/material-module';
+import {MatCardModule, MatGridListModule, MatProgressSpinnerModule} from '@angular/material';
 
 
 @NgModule({
@@ -11,7 +10,9 @@ import { DemoMaterialModule } from 'src/app/material-module';
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    DemoMaterialModule
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
   ]
 })
 export class DashboardModule { }
--- src/app/Modules/manage-edit/file-manager/file-manager.module.ts
+++ src/app/Modules/manage-edit/file-manager/file-manager.module.ts
@@ -1,19 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileManagerComponent } from './file-manager.component';
-import { DemoMaterialModule } from 'src/app/material-module';
 import {FilelistComponent} from '../../../Layouts/filemanager/filelist/filelist.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DialogBoxComponent } from './toolbar/dialog-box/dialog-box.component';
 import {FormsModule} from '@angular/forms';
 import {FilelistMobileComponent} from '../../../Layouts/filemanager/filelist-mobile/filelist-mobile.component';
+import {
+  _MatMenuDirectivesModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule, MatInputModule, MatMenuModule, MatProgressBarModule, MatSortModule, MatTableModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [FileManagerComponent, FilelistComponent, ToolbarComponent, DialogBoxComponent, FilelistMobileComponent],
   imports: [
     CommonModule,
-    DemoMaterialModule,
     FormsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule,
+    MatProgressBarModule,
   ],
   exports: [
     FileManagerComponent,
